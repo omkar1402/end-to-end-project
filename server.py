9ba6c2a6011068553cbe0ffca70b7f205001e488
omkar1402 <omkarane84@gmail.com>
2024-06-04 22:48:42 +0530

    Inserted test data in Mongodb using POSt request

--- server.py
+++ server.py
@@ -22,48 +22,42 @@ def form():
 # Add data to MongoDB route 
 @app.route('/add_data', methods=['POST']) 
 def add_data(): 
-    # from pymongo import MongoClient 
+	try: 
+		conn = MongoClient() 
+		print("Connected successfully!!!") 
+	except: 
+		print("Could not connect to MongoDB") 
 
-# try: 
-# 	conn = MongoClient() 
-# 	print("Connected successfully!!!") 
-# except: 
-# 	print("Could not connect to MongoDB") 
+	# database 
+	db = conn.Test 
 
-# database 
-db = conn.Test 
+	# Created or Switched to collection names: my_gfg_collection 
+	collection = db.test_coll 
 
-# Created or Switched to collection names: my_gfg_collection 
-collection = db.test_coll 
+	data=request.json
+	collection.insert_one(data)
+	
+	# emp_rec1 = { 
+	# 		"name":"Mr.Geek", 
+	# 		"eid":24, 
+	# 		"location":"delhi"
+	# 		} 
 
-emp_rec1 = { 
-		"name":"Mr.Geek", 
-		"eid":24, 
-		"location":"delhi"
-		} 
-# emp_rec2 = { 
-# 		"name":"Mr.Omkar", 
-# 		"eid":14, 
-# 		"location":"delhi"
-# 		} 
+	# # Insert Data 
+	# rec_id1 = collection.insert_one(emp_rec1) 
 
-# Insert Data 
-rec_id1 = collection.insert_one(emp_rec1) 
-# rec_id2 = collection.insert_one(emp_rec2) 
+	# print("Data inserted with record ids",rec_id1) 
 
-print("Data inserted with record ids",rec_id1) 
-
-# Printing the data inserted 
-cursor = collection.find() 
-for record in cursor: 
-	print(record) 
-    # Get data from request 
-    data = request.json 
-  
-    # Insert data into MongoDB 
-    collection.insert_one(data) 
-  
-    return 'Data added to MongoDB'
+	# # Printing the data inserted 
+	# cursor = collection.find() 
+	# for record in cursor: 
+	# 	print(record) 
+	# 	# Get data from request 
+	# 	data = request.json 
+	
+    # # Insert data into MongoDB 
+    # collection.insert_one(data) 
+	return 'Data added to MongoDB'
   
 if __name__ == "__main__":
     app.run()
